3d729ca1d86e0791671f48c93020abaef27665be
Chahinaz <chahinaz@dindane.com>
2018-07-27 15:17:28 +0200

    [WIP] Create css file, adding App.vue

---
+++ components/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <toolbar></toolbar>
+        <notes-list></notes-list>
+        <editor></editor>
+    </div>
+</template>
+
+
+<script>
+    import Toolbar from './Toolbar.vue'
+    import NotesList from './NotesList.vue'
+    import Editor from './Editor.vue'
+
+    import {addNote, addFavoriteNode, deleteNote} from '../node_modules/vuex/actions'
+
+    export default{
+        vuex: {
+            getters: {
+                activeNote: state => state.activeNote
+            },
+            actions: {
+                addNote,
+                addFavoriteNote,
+                deleteNote
+            }
+        },
+        components: {
+            Toolbar,
+            NotesList,
+            Editor
+        }
+    }
+</script>
