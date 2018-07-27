5c110f2d4f54f6bb74ffd8d18a92da1ef7c427c6
Chahinaz <chahinaz@dindane.com>
2018-07-27 15:24:57 +0200

    [WIP] create ToolBar

---
+++ components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="toolbar">
+        <i @click="addNote" class="glyphicon-plus"></i>
+        <i @click="addFavoriteNote" class="glyphicon glyphicon-star"
+            :class="{starred: activeNote.favorite}"></i>
+        <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>
+    </div>
+</template>
+
+<script>
+    import {addNote, addFavoriteNote, deleteNote} from '../node_modules/vuex/actions'
+
+    export default {
+        vuex: {
+            getters: {
+                activeNote: state => state.activeNote
+            },
+            actions: {
+                addNote,
+                addFavoriteNote,
+                deleteNote
+            }
+        }
+    }
+
+</script>
